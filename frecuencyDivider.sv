9c66d1a62964ef1cffb7581d2f3ceb8a27d971c3
Arturo Mora <luartmgr@gmail.com>
2018-05-20 03:16:51 -0600

    Ultima fix naruto vs sasuke

--- frecuencyDivider.sv
+++ frecuencyDivider.sv
@@ -2,10 +2,8 @@ module frequencyDivider (
 	input logic i_clk, i_rst,
 	output logic o_clk);
 	
-	logic [1:0] cntClk;
-	
-	counter#(2) c0(i_clk, i_rst, 1, cntClk);
-	
-	assign o_clk = (cntClk < 2'd2) ? 1'b0 : 1'b1;
+	always_ff @(posedge i_clk)
+		if (i_rst) o_clk <= 0;
+		else o_clk <= ~o_clk;
 	
 endmodule
--- topV.sv
+++ topV.sv
@@ -1,14 +1,13 @@
 module topV (
     input logic clk, rst,
     output logic[7:0] r, g, b,
-	 output logic o_hs, o_vs, o_sync, o_blank);
+	 output logic o_hs, o_vs, o_sync, o_blank, o_clk);
 	 
 	 logic [9:0] x, y;
-	 logic p_clk = 0;
 	 
-	 frequencyDivider ffd(clk, rst, p_clk);
+	 frequencyDivider ffd(clk, rst, o_clk);
 	 
-	 vgaController v0(p_clk, rst, o_hs, o_vs, o_sync, o_blank, x, y);
+	 vgaController v0(o_clk, rst, o_hs, o_vs, o_sync, o_blank, x, y);
 	 
 	 assign r = o_blank ? 8'd255 : 8'd0;
 	 assign g = o_blank ? 8'd255 : 8'd0;
