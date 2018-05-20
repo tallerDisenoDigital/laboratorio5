79dcf331e16eb5eb80e7197a1365b779733b0a0b
Arturo Mora <luartmgr@gmail.com>
2018-05-20 01:13:16 -0600

    cambio de sync

--- vgaController.sv
+++ vgaController.sv
@@ -2,22 +2,23 @@
 											HFP 	  = 10'd15,
 											HSYN    = 10'd95,
 											HBP     = 10'd48,
-											HMAX    = HACTIVE + HFP + HSYN + HBP,
-											HSS     = HACTIVE + HFP,
-											HSE     = HACTIVE + HFP + HSYN,
+											HSS     = HSYN + HBP,
+											HSE     = HSYN + HBP + HACTIVE,
+											HMAX    = HSYN + HBP + HACTIVE + HFP,
 											
 											VACTIVE = 10'd480,
 											VFP     = 10'd10,
 											VSYN    = 10'd2,
 											VBP     = 10'd33,
-											VMAX    = VACTIVE + VFP + VSYN + VBP,
-											VSS     = VACTIVE + VFP,
-											VSE     = VACTIVE + VFP + VSYN)
+											VSS     = VSYN + VBP,
+											VSE     = VSYN + VBP + VACTIVE,
+											VMAX    = VSYN + VBP + VACTIVE + VFP)
 	(input logic i_clk, i_rst,
 	output logic o_hs, o_vs, o_sync_n, o_blank_n,
 	output logic [9:0] o_x, o_y); 	
 	
-	logic hRstF, vRstF, hSyncSF, hSyncEF, vSyncSF, vSyncEF,  hBlankF, vBlankF = 0;
+	logic hRstF, vRstF, hDS, hDE, vDS, vDE = 0;
+	
 	logic [9:0] hCnt, vCnt;
 	
 	//	Pixel Counter
@@ -28,28 +29,22 @@
 	comparator#(10) hMaxComparator(.a(hCnt), .b(HMAX - 1), .gte(hRstF));
 	comparator#(10) vMaxComparator(.a(vCnt), .b(VMAX - 1), .gte(vRstF));
 	
-	
-	//	H-Sync Comparator
-	comparator#(10) hSSComparator(.a(hCnt), .b(HSS), .gte(hSyncSF));
-	comparator#(10) hSEComparator(.a(hCnt), .b(HSE), .lt(hSyncEF));
-	
-	//	V-Sync Comparator
-	comparator#(10) vVSComparator(.a(vCnt), .b(VSS), .gte(vSyncSF));
-	comparator#(10) vSEComparator(.a(vCnt), .b(VSE), .lt(vSyncEF));
+	//	Sync Signals (Low Active)
+	comparator#(10) hSSComparator(.a(hCnt), .b(HSYN), .gte(o_hs));
+	comparator#(10) vSEComparator(.a(vCnt), .b(VSYN), .gte(o_vs));
 	
 	//	Blank Comparator
-	comparator#(10) bHComparator(.a(hCnt), .b(HACTIVE), .lt(hBlankF));
-	comparator#(10) bVComparator(.a(vCnt), .b(VACTIVE), .lt(vBlankF));
+	comparator#(10) hDisplayStartComparator(.a(hCnt), .b(HSS), .gte(hDS));
+	comparator#(10) hDisplayEndComparator(.a(hCnt), .b(HSE), .lt(hDE));
 	
-	//	Sync Active Low Signals
-   assign o_hs = ~(hSyncSF & hSyncEF);
-   assign o_vs = ~(vSyncSF & vSyncEF);
-	assign o_sync_n = o_hs & o_vs;
+	comparator#(10) vDisplayStartComparator(.a(vCnt), .b(VSS), .gte(vDS));
+	comparator#(10) vDisplayEndComparator(.a(vCnt), .b(VSE), .lt(vDE));
 	
-	assign o_blank_n = hBlankF & vBlankF;
+	assign o_sync_n = 1'b0; //o_hs & o_vs;
 	
+	assign o_blank_n = hDS & hDE & vDS & vDE;
 	
-   assign o_x = (hBlankF) ? hCnt : HACTIVE;
-   assign o_y = (vBlankF) ? vCnt : VACTIVE;
+   assign o_x = hCnt;
+   assign o_y = vCnt;
 	
 endmodule
